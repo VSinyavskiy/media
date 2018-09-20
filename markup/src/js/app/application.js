4ab7b6efc50371e1dd9a0159424bb51ca3a2ec7d
Roman Migdalyonok <mindhalt@gmail.com>
2018-09-20 12:39:49 +0300

    [markup] game integration fixes

--- markup/src/js/app/application.js
+++ markup/src/js/app/application.js
@@ -4,9 +4,11 @@ import Sliders from './Sliders';
 import Parallax from './Parallax';
 import FormControls from './FormControls';
 import CopyCode from './CopyCode';
+import Game from './Game';
 
 new Main();
 new FormControls();
 new CopyCode();
 export const sliders = new Sliders();
 export const parallax = new Parallax();
+export const game = new Game();
