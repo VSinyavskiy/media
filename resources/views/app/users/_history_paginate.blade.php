53fc537b1890d42e43ec3c88e403d797d05b19fe
Vlad Sinyavskiy <vlad@Vlads-iMac.local>
2018-09-21 14:47:15 +0300

    Added minutes to history scoring

--- resources/views/app/users/_history_paginate.blade.php
+++ resources/views/app/users/_history_paginate.blade.php
@@ -2,7 +2,7 @@
 	<?php $countPoints = $key == 0 ? $leftTotalPoints : $countPoints - $point->points ?>
     <li class="doners-block__item">
         <div class="doner-dude">
-            <div class="doner-dude__action"><span class="doner-dude__date">{{ $point->scoring_at->format('d.m.Y') }}</span>{{ $point->event_name }}</div>
+            <div class="doner-dude__action"><span class="doner-dude__date">{{ $point->scoring_at->format('d.m.Y H:i:s') }}</span>{{ $point->event_name }}</div>
             <div class="doner-dude__score">{{ $countPoints }}</div>
         </div>
     </li>
