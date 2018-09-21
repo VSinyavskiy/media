f52e31250191dbdc7c7c21614563caed2dd8f788
Vlad Sinyavskiy <vlad@Vlads-iMac.local>
2018-09-21 15:08:34 +0300

    Fixed history view

--- resources/views/app/users/_history_paginate.blade.php
+++ resources/views/app/users/_history_paginate.blade.php
@@ -1,5 +1,8 @@
 @foreach ($points as $key => $point)
-	<?php $countPoints = $key == 0 ? $leftTotalPoints : $countPoints - $point->points ?>
+	<?php 
+		$countPoints 	 = $key == 0 ? $leftTotalPoints : $countPoints - $previousePoints;
+		$previousePoints = $point->points;
+	?>
     <li class="doners-block__item">
         <div class="doner-dude">
             <div class="doner-dude__action"><span class="doner-dude__date">{{ $point->scoring_at->format('d.m.Y H:i:s') }}</span>{{ $point->event_name }}</div>
